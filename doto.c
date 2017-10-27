288e4c4e0022db72abe0a757e9187a1be8b4b109
rancho <rancho@arch>
2017-10-27 21:33:34 +0300

    + add print work, read from file does not work

--- doto.c
+++ doto.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <fcntl.h>
-#include <strings.h>
+#include <string.h>
 
 struct entry {
 	int year;
 	int month;
 	int day;
-	struct *next;
+	struct entry *next;
 	char descr[50];
-}
+};
 
-struct new_entry() 
+struct entry *new_entry()
 {
 	struct entry  *new;
 	new = malloc(sizeof(struct entry));
@@ -22,6 +23,7 @@ struct new_entry()
 		fprintf(stderr, "Out of memory\n");
 		exit(1);
 	}
+	new->next = NULL;
 	return new;
 }
 
@@ -39,7 +41,7 @@ char display_menu()
 	printf("\t0: Quit doto\n");
 	printf("\t1: Print today stuff \n");
 	printf("\t2: Print week stuff \n");
-	printf("\t3: Print month stuff \n");
+	printf("\t3: Print all stuff \n");
 	printf("\t4: Edit a todo\n");
 	printf("\t5: Add a todo\n");
 	printf("\t6: Mark a todo as done\n");
@@ -57,8 +59,9 @@ int open_mem_file()
 	if(!homedir) 
 		homedir = getpwuid(getuid())->pw_dir;
 	strcpy(memfile, homedir);
-	strcat(memfile, ".doto_file");
-	fd = open(memfile, O_RDWR | O_TRUNC | O_CREAT);
+	strcat(memfile, "/.doto_file");
+	fprintf(stderr, "Hi: %s\n", memfile);
+	fd = open(memfile, O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if(fd == -1) {
 		perror("memory file");
 		exit(1);
@@ -69,18 +72,43 @@ int open_mem_file()
 void read_nbytes(int fd, void *buf, ssize_t n)
 {
 	ssize_t bts_read = 0;
+	int tries = 0;
 	while(n) {
 		bts_read = read(fd, buf + bts_read, n);
 		if(bts_read == -1) {
 			perror("read memory file");
 			exit(1);
 		}
-		n -= bts_read;
+		if(!bts_read) {
+			tries++;
+			if(tries == 3) {
+				tries -= 3;
+				memcpy(buf, &tries, sizeof(int));
+				break;
+			}
+		} else {
+			n -= bts_read;
+			tries = 0;
+		}
 	}
 	return;
 }
 
-struct entry read_entry(int fd)
+void write_nbytes(int fd, void *buf, ssize_t n)
+{
+	ssize_t bts_written = 0;
+	while(n) {
+		bts_written = write(fd, buf + bts_written, n);
+		if(bts_written == -1) {
+			perror("read memory file");
+			exit(1);
+		}
+		n -= bts_written;
+	}
+	return;
+}
+
+struct entry *read_entry(int fd)
 {
 	struct entry *n_entry = new_entry();
 	ssize_t read_bts;
@@ -91,27 +119,142 @@ struct entry read_entry(int fd)
 	return n_entry;
 }
 
-struct entry read_all_entries(int fd, int cnt)
+struct entry *read_all_entries(int fd, int cnt)
 {
 	if(!cnt)
 		return NULL;
 	struct entry *first, **dfirst;
 	int i;
 	first = read_entry(fd);
-	**dfirst = &(first->next);
+	dfirst = &(first->next);
 	for(i = 0; i < cnt - 1; i++) {
 		*dfirst = read_entry(fd);
-		**dfirst = &((*dfirst)->next)
+		dfirst = &((*dfirst)->next);
 	}
 	*dfirst = NULL;
 	return first;
 }
 
+void print_today(struct entry *ens)
+{
+	struct entry *tmp = ens;
+	time_t t = time(NULL);
+	struct tm time = *localtime(&t);
+	printf("For today you have to:\n");
+	while(tmp) {
+		if(tmp->year > time.tm_year + 1900)
+			break;
+		if(tmp->month > time.tm_mon + 1)
+			break;
+		if(tmp->day > time.tm_mday)
+			break;
+		printf("\t %s\n", tmp->descr);
+		tmp = tmp->next;
+	}
+	printf("---------------------------\n End of tasks for today\n");
+	return;
+}
+
+void print_week(struct entry *ens)
+{
+	struct entry *tmp = ens;
+	time_t t = time(NULL);
+	struct tm time = *localtime(&t);
+	printf("For this week you have to:\n");
+	while(tmp) {
+		if(tmp->year > time.tm_year + 1900)
+			break;
+		if(tmp->month > time.tm_mon + 1)
+			break;
+		if(tmp->day > time.tm_mday)
+			break;
+		printf("\t %s\n", tmp->descr);
+		tmp = tmp->next;
+	}
+	printf("---------------------------\n End of tasks for this week\n");
+	return;
+}
+
+void print_all(struct entry *ens)
+{
+	struct entry *tmp = ens;
+	printf("All tasks:\n");
+	while(tmp) {
+		printf("---:\t %s\n", tmp->descr);
+		tmp = tmp->next;
+	}
+	printf("---------------------------\n End of tasks\n");
+	return;
+}
+
+struct entry *add(struct entry *ens)
+{
+	struct entry *tmp = ens, **dtmp = &ens, *nentry = new_entry();
+	printf("Which year the task ends: ");
+	scanf("%d", &(nentry->year));
+	getchar();
+	printf("Which month the task ends: ");
+	scanf("%d", &(nentry->month));
+	getchar();
+	printf("Which day the task ends: ");
+	scanf("%d", &(nentry->day));
+	getchar();
+	printf("Add a description (50 chars max): ");
+	fgets(nentry->descr, sizeof(nentry->descr), stdin);
+	//scanf("%s", nentry->descr);
+	if(!tmp)
+		return nentry;
+	while(tmp) {
+		if(nentry->year < tmp->year) {
+			break;
+		} else if (nentry->year > tmp->year) {
+			dtmp = &(tmp->next);
+			tmp = tmp->next;
+		}else {
+			if(nentry->month < tmp->month) {
+				break;
+			} else if (nentry->month > tmp->month) {
+				dtmp = &(tmp->next);
+				tmp = tmp->next;
+			}else {
+				if(nentry->day <= tmp->day) {
+					break;
+				} else if (nentry->day > tmp->day) {
+					dtmp = &(tmp->next);
+					tmp = tmp->next;
+				}
+			}
+		}
+	}
+	nentry->next = tmp;
+	*dtmp = nentry;
+	return ens;
+}
+
+void close_mem_file(struct entry *ens, int f)
+{
+
+	struct entry *tmp = ens;
+	while(tmp) {
+		write_nbytes(f, &(tmp->year), sizeof(int));
+		write_nbytes(f, &(tmp->month), sizeof(int));
+		write_nbytes(f, &(tmp->day), sizeof(int));
+		write_nbytes(f, &(tmp->descr), sizeof(tmp->descr));
+		tmp = tmp->next;
+	}
+	if(close(f) == -1) {
+		perror("close memory file:");
+		exit(1);
+	}
+	return;
+}
+
 int main()
 {
 	char choice;
-	int fd, entry_cnt;
+	int fd, entry_cnt = 0;
 	struct entry *entries;
+	fd = open_mem_file();
 	read_nbytes(fd, &entry_cnt, sizeof(entry_cnt));
 	entries = read_all_entries(fd, entry_cnt);
 	while(1) {
@@ -119,26 +262,29 @@ int main()
 		switch(choice) {
 			case 0 : 
 				printf("bye\n");
+				write_nbytes(fd, &entry_cnt, sizeof(entry_cnt));
+				close_mem_file(entries, fd);
 				return 0;
 			case 1:
-				//print_today();
-				printf("print today\n");
+				print_today(entries);
+				//printf("print today\n");
 				break;
 			case 2:
-				//print_week();
-				printf("print week\n");
+				print_week(entries);
+				//printf("print week\n");
 				break;
 			case 3:
-				//print_month();
-				printf("print month\n");
+				print_all(entries);
+				//printf("print month\n");
 				break;
 			case 4:
 				//edit();
 				printf("edit\n");
 				break;
 			case 5:
-				//add();
-				printf("add\n");
+				entries = add(entries);
+				entry_cnt++;
+				//printf("add\n");
 				break;
 			case 6:
 				//done();
