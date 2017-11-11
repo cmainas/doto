ce82bba66ef55571abc24af3f1607396874d029f
rancho <rancho@arch>
2017-11-11 13:49:15 +0200

    + print today and change values from int to char

--- doto.c
+++ doto.c
@@ -9,8 +9,9 @@
 
 struct entry {
 	int year;
-	int month;
-	int day;
+	char month;
+	char day;
+	char done;
 	struct entry *next;
 	char descr[50];
 };
@@ -30,13 +31,13 @@ struct entry *new_entry()
 char display_menu()
 {
 	time_t t = time(NULL);
-	struct tm *time = localtime(&t);
+	struct tm *tm = localtime(&t);
 	char action = -1;
 	printf("Hello to doto menu\n");
-	printf("Today is %s\n", asctime(time));
-//	printf("Today is: %d-%d-%d %d:%d:%d\n", time.tm_mday, 
-//			time.tm_mon++, time.tm_year + 1900, time.tm_hour,
-//		       	time.tm_min, time.tm_sec);
+	printf("Today is %s\n", asctime(tm));
+//	printf("Today is: %d-%d-%d %d:%d:%d\n", tm.tm_mday, 
+//			tm.tm_mon++, tm.tm_year + 1900, tm.tm_hour,
+//		       	tm.tm_min, tm.tm_sec);
 	printf("Menu: \n");
 	printf("\t0: Quit doto\n");
 	printf("\t1: Print today stuff \n");
@@ -51,7 +52,7 @@ char display_menu()
 	return action - '0';
 }
 
-int open_mem_file()
+int open_mem_file(int flags)
 {
 	const char *homedir = getenv("HOME");
 	char memfile[50];
@@ -61,7 +62,7 @@ int open_mem_file()
 	strcpy(memfile, homedir);
 	strcat(memfile, "/.doto_file");
 	//fprintf(stderr, "Hi: %s\n", memfile);
-	fd = open(memfile, O_RDWR | O_CREAT, 0644);
+	fd = open(memfile, flags, 0644);
 	if(fd == -1) {
 		perror("memory file");
 		exit(1);
@@ -105,7 +106,7 @@ void write_nbytes(int fd, void *buf, ssize_t n)
 	while(n) {
 		bts_written = write(fd, buf + bts_written, n);
 		if(bts_written == -1) {
-			perror("read memory file");
+			perror("write memory file");
 			exit(1);
 		}
 		n -= bts_written;
@@ -118,8 +119,9 @@ struct entry *read_entry(int fd)
 	struct entry *n_entry = new_entry();
 	ssize_t read_bts;
 	read_nbytes(fd, &(n_entry->year), sizeof(int));
-	read_nbytes(fd, &(n_entry->month), sizeof(int));
-	read_nbytes(fd, &(n_entry->day), sizeof(int));
+	read_nbytes(fd, &(n_entry->month), sizeof(char));
+	read_nbytes(fd, &(n_entry->day), sizeof(char));
+	read_nbytes(fd, &(n_entry->done), sizeof(char));
 	read_nbytes(fd, &(n_entry->descr), 50*sizeof(char));
 	return n_entry;
 }
@@ -144,19 +146,24 @@ void print_today(struct entry *ens)
 {
 	struct entry *tmp = ens;
 	time_t t = time(NULL);
-	struct tm time = *localtime(&t);
-	printf("For today you have to:\n");
+	struct tm *tm = localtime(&t);
+	int cnt=0;
+	printf("All tasks:\n");
+	printf("-------------------------------------\n"
+		"|id	|Day	|Month	|Year	|Done?	|Description|\n"
+		"-------------------------------------\n");
 	while(tmp) {
-		if(tmp->year > time.tm_year + 1900)
+		if(tmp->year > (tm->tm_year + 1900))
 			break;
-		if(tmp->month > time.tm_mon + 1)
+		if(tmp->month > (tm->tm_mon + 1))
 			break;
-		if(tmp->day > time.tm_mday)
+		if(tmp->day > tm->tm_mday)
 			break;
-		printf("\t %s\n", tmp->descr);
+		printf("|%d\t|%d\t|%d\t|%d\t|%d\t|%s", cnt++, tmp->day, 
+				tmp->month, tmp->year, tmp->done, tmp->descr);
+		printf("-------------------------------------\n");
 		tmp = tmp->next;
 	}
-	printf("---------------------------\n End of tasks for today\n");
 	return;
 }
 
@@ -182,13 +189,18 @@ void print_week(struct entry *ens)
 
 void print_all(struct entry *ens)
 {
+	int cnt = 0;
 	struct entry *tmp = ens;
 	printf("All tasks:\n");
+	printf("-------------------------------------\n"
+		"|Id	|Day	|Month	|Year	|Done?	|Description|\n"
+		"-------------------------------------\n");
 	while(tmp) {
-		printf("---:\t %s\n", tmp->descr);
+		printf("|%d\t|%d\t|%d\t|%d\t|%d\t|%s", cnt++, tmp->day, 
+				tmp->month, tmp->year, tmp->done, tmp->descr);
+		printf("-------------------------------------\n");
 		tmp = tmp->next;
 	}
-	printf("---------------------------\n End of tasks\n");
 	return;
 }
 
@@ -206,6 +218,7 @@ struct entry *add(struct entry *ens)
 	getchar();
 	printf("Add a description (50 chars max): ");
 	fgets(nentry->descr, sizeof(nentry->descr), stdin);
+	nentry->done = 0;
 	//scanf("%s", nentry->descr);
 	if(!tmp)
 		return nentry;
@@ -236,21 +249,30 @@ struct entry *add(struct entry *ens)
 	return ens;
 }
 
-void close_mem_file(struct entry *ens, int f)
+void close_mem_file(int f)
 {
 
-	struct entry *tmp = ens;
-	while(tmp) {
-		write_nbytes(f, &(tmp->year), sizeof(int));
-		write_nbytes(f, &(tmp->month), sizeof(int));
-		write_nbytes(f, &(tmp->day), sizeof(int));
-		write_nbytes(f, &(tmp->descr), sizeof(tmp->descr));
-		tmp = tmp->next;
-	}
 	if(close(f) == -1) {
 		perror("close memory file:");
 		exit(1);
 	}
+	return;
+}
+
+void update_mem_file(int cnt, struct entry *ens)
+{
+	int f = open_mem_file(O_WRONLY);
+	struct entry *tmp = ens;
+	write_nbytes(f, &cnt, sizeof(int));
+	while(tmp) {
+		write_nbytes(f, &(tmp->year), sizeof(int));
+		write_nbytes(f, &(tmp->month), sizeof(char));
+		write_nbytes(f, &(tmp->day), sizeof(char));
+		write_nbytes(f, &(tmp->done), sizeof(char));
+		write_nbytes(f, &(tmp->descr), sizeof(tmp->descr));
+		tmp = tmp->next;
+	}
+	close_mem_file(f);
 	return;
 }
 
@@ -259,17 +281,17 @@ int main()
 	char choice;
 	int fd, entry_cnt = 0;
 	struct entry *entries;
-	fd = open_mem_file();
+	fd = open_mem_file(O_RDONLY | O_CREAT);
 	read_nbytes(fd, &entry_cnt, sizeof(entry_cnt));
-	printf("bts: %d\n ", entry_cnt);
 	entries = read_all_entries(fd, entry_cnt);
+	close_mem_file(fd);
+	printf("bts: %d\n ", entry_cnt);
 	while(1) {
 		choice = display_menu();
 		switch(choice) {
 			case 0 : 
 				printf("bye: %d\n", entry_cnt);
-				write_nbytes(fd, &entry_cnt, sizeof(entry_cnt));
-				close_mem_file(entries, fd);
+				update_mem_file(entry_cnt, entries);
 				return 0;
 			case 1:
 				print_today(entries);
